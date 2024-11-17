9cd145c53e7b1d4d7b0d84bc2e7a8c23f923cc1f
FaridAliyev <feliyev636@gmail.com>
2024-11-17 11:02:19 +0400

    update slider

--- Klift/src/components/Carousel.jsx
+++ Klift/src/components/Carousel.jsx
@@ -6,20 +6,17 @@ function Carousel() {
   const [currentSlide, setCurrentSlide] = useState(0);
   const slides = [slide1, slide2];
 
-  // Auto-play functionality
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentSlide((prev) => (prev + 1) % slides.length);
-    }, 3000); // Change slide every 3 seconds
+    }, 3000);
 
-    return () => clearInterval(interval); // Cleanup interval on unmount
+    return () => clearInterval(interval);
   }, [slides.length]);
 
   return (
     <div className="relative w-full h-screen bg-gray-900">
-      {/* Slides Wrapper */}
       <div className="absolute inset-0 overflow-hidden">
-        {/* Slides */}
         {slides.map((slide, index) => (
           <div
             key={index}
@@ -35,14 +32,12 @@ function Carousel() {
         ))}
       </div>
 
-      {/* Black Overlay */}
       <div className="absolute inset-0 bg-black bg-opacity-30"></div>
 
-      {/* Content */}
       <div className="relative flex flex-col items-center justify-center h-full text-center text-white">
-        <div className="p-6 rounded-lg w-3/5">
+        <div className="rounded-lg">
           <h1 className="text-5xl font-bold mb-4">Transforming Spaces with Elegance</h1>
-          <p className="text-lg mb-8">
+          <p className="text-lg mb-8 w-slider-width ms-auto me-auto">
           We design furniture that complements your lifestyle while ensuring exceptional comfort and durability. Discover pieces that transform your space into a haven.
           </p>
           <button className="px-6 py-3 bg-white bg-opacity-0 border rounded-full transition-colors duration-300 text-white hover:bg-white hover:text-black">
--- Klift/tailwind.config.js
+++ Klift/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        'slider-width':'80%'
+      }
+    },
   },
   plugins: [],
 }
